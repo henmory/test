658b09f9286c7a723e08e55417e4afb6a1d21932
henmory <hanmh87@163.com>
2018-04-25 17:44:34 +0800

    继续提交，因为有些文件没有提交上去

--- sendbroadcast/src/main/java/com/digiarty/sendbroadcast/MainActivity.java
+++ sendbroadcast/src/main/java/com/digiarty/sendbroadcast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.digiarty.sendbroadcast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,12 +17,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //发送有权限广播
+                sendBroadcast(new Intent("digiarty.phoneassistant.startservice.broadcast"), "com.digiarty.phoneassistant.permission.receive_broadcast");
+//                sendBroadcast(new Intent("digiarty.phoneassistant.startservice.broadcast"));
             }
         });
     }
