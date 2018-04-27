c4596e92276012c1fdd5052effcedbf71998559b
henmory <hanmh87@163.com>
2018-04-27 17:54:31 +0800

    上次没有提交任何内容，因为改变的内容没有加到staged中，这次重新提交

--- log/src/main/java/com/example/log/MainActivity.java
+++ log/src/main/java/com/example/log/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.log;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -13,26 +14,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.log.file.FileHelper;
+import com.example.log.global.GlobalApplication;
+
+import java.io.File;
+
 /**
- * @date
- *
- * @author henmory
- *
  * @param
- *
+ * @author henmory
+ * @date
  * @return
- *
- *
  * @description 文件权限的获取需要在activity中才行，但是我们的软件没有activity
- *
  */
 
-public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
+    private final static String TAG = MainActivity.class.getSimpleName();
     private TextView textView;
     //定义日志对象
 //    private static Logger logger = LoggerFactory.getLogger(MainActivity.class);
     private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,34 +42,30 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         createFileOnExternalStorage();
     }
 
-    //业务逻辑
     private void createFileOnExternalStorage() {
 
         //检查权限是否授予
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             // Permission is already available, 开始创建文件
+            Log.d(TAG, "createFileOnExternalStorage: 用户有创建文件的权限，直接创建文件");
             createLogDirectoryInPublicDocuments();
             finish();
             //没有授予，请求权限
         } else {
             // Permission is missing and must be requested.
+            Log.d(TAG, "createFileOnExternalStorage: 用户没有创建文件的权限，开始请求权限");
             requestWriteExternalStoragePermission();
         }
 
     }
 
 
-    /**
-     * Requests the {@link android.Manifest.permission#CAMERA} permission.
-     * If an additional rationale should be displayed, the user has to launch the request from
-     * a SnackBar that includes additional information.
-     */
     private void requestWriteExternalStoragePermission() {
 
         // 第一次不会运行里面代码，会走else请求权限，如果用户选择拒绝，那么当再次运行这个代码时，会走这里的代码
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             //再次提醒用户，必须选择允许，否则不能操作
-            showNormalDialog();
+            showWarningIfRefusePermission();
 
         } else {
             //请求权限
@@ -82,29 +80,24 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // BEGIN_INCLUDE(onRequestPermissionsResult)
         if (requestCode == PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE) {
             if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission has been granted. Start camera preview Activity.
+                // Permission has been granted.
+                Log.d(TAG, "onRequestPermissionsResult: 用户授权，可以创建了");
                 createLogDirectoryInPublicDocuments();
                 finish();
             } else {
                 // Permission request was denied.
-                showNormalDialog();
+                Log.d(TAG, "onRequestPermissionsResult: 用户拒绝创建，告诉用户如果不同意，则不能使用app");
+                showWarningIfRefusePermission();
 
             }
         }
     }
 
 
+    private void showWarningIfRefusePermission() {
 
-
-    private void showNormalDialog(){
-        /* @setIcon 设置对话框图标
-         * @setTitle 设置对话框标题
-         * @setMessage 设置对话框消息提示
-         * setXXX方法返回Dialog对象，因此可以链式设置属性
-         */
         final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
         normalDialog.setIcon(R.drawable.ic_launcher_background);
         normalDialog.setTitle("警告");
@@ -123,20 +116,21 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                            finish();
+                        Log.d(TAG, "onClick: 用户再度拒绝授权，退出应用");
+                        finish();
                     }
                 });
         // 显示
         normalDialog.show();
     }
 
-    private void createLogDirectoryInPublicDocuments(){
+    private void createLogDirectoryInPublicDocuments() {
 
         Log.d("MainActivity", "createLogDirectoryInPublicDocuments: 开始创建");
-//        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
-//        String abPath = path.getAbsolutePath() + "/" + GlobalApplication.getGlobalPackageName();
-//        System.out.println("log path = " + abPath);
-//        FileHelper.createDir(abPath);
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        String abPath = path.getAbsolutePath() + "/" + GlobalApplication.getGlobalPackageName();
+        System.out.println("log path = " + abPath);
+        FileHelper.createDir(abPath);
 
     }
 }
